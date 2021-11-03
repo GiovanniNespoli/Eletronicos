6e9312108281705341127053ec616c0c8e9a1743
unknown <ggnespoli@gmail.com>
2021-11-03 00:52:04 -0300

    n tinha nenhuma branch alem da main

--- tcc/src/pages/drawer/drawer.js
+++ tcc/src/pages/drawer/drawer.js
@@ -4,6 +4,9 @@ import React, { Component } from 'react';
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 
 import Home from '../Home/home'
+import Login from '../user/login'
+import Edicao from '../user/edicao'
+import Lista from '../lista_lojas/lista';
 
 const Drawer = createDrawerNavigator()
 
@@ -19,20 +22,29 @@ export default class drawer extends Component{
         return(
             <Drawer.Navigator
             screenOptions={{
-                headerTitle : 'Home',
+                headerTitle : 'Bem vindo',
+                headerTintColor: 'white',
+                headerTitleAlign: 'center',
                 headerStyle : {
-                    backgroundColor : 'green'
+                    backgroundColor : '#00873B',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                 },
-                
+
                 drawerActiveTintColor : '#FFF',
                 drawerInactiveTintColor : '#FFF',
                 drawerPosition : 'left',
                 drawerStyle : {
-                  backgroundColor :'#18384A',
+                  backgroundColor :'#00873B',
                   borderRadius : 20,
+                  height: '50%',
+                  width: '50%',
                 },
-              }}>
+            }}>
+                <Drawer.Screen name="Lista" component={Lista}/>
+                <Drawer.Screen name="Edição" component={Edicao}/>
                 <Drawer.Screen name="Home" component={Home}/>
+                <Drawer.Screen name="Login" component={Login}/>
             </Drawer.Navigator>
         )
     }
